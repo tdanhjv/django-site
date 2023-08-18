14dc2064c66050be2369f057806d137ed609d401
Tran Duy Anh <anh.td.jv@gmail.com>
2023-08-18 18:44:00 +0700

    Django Tutorial Part 3: Using models update 16h44 21/08

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -23,5 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
+    path('', RedirectView.as_view(url='admin/')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
